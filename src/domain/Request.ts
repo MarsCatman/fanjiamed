c3dc6caa2f6d4a42e9ac92b6afbe749ee77b9951
songhaobin <songhaobin@fanjia.net>
2020-11-25 17:20:55 +0800

    打包

--- src/domain/Request.ts
+++
@@ -1,9 +0,0 @@
-export type ReponseResult = {
-    ResultCode: number;
-    status:any;
-    tipsinfo:any;
-    Data: any;
-    MsgCode:string;
-    msg:string;
-  }
-  
--- src/plugins/layers.ts
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import FjAlert from '@/components/layers';
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-Vue.use(VueAwesomeSwiper)
-
-Vue.use(FjAlert)
